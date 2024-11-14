50badd0c4f1048f578c273d7b488ebf19bff9aee
orwoods <vanya@techjedi.co>
2024-11-14 02:29:53 +0100

    yarn to npm

--- bin/build-grpc-cli.js
+++ bin/build-grpc-cli.js
@@ -6,4 +6,4 @@ const { execSync } = require('child_process');
 const cwd = resolve(__dirname, '..');
 const grpcDir = process.argv[2] || 'src/grpc';
 
-execSync(`GRPC_DIR=${grpcDir} yarn build-grpc`, { cwd, stdio: 'inherit' });
+execSync(`GRPC_DIR=${grpcDir} npm run build-grpc`, { cwd, stdio: 'inherit' });
