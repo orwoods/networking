fec4aa1f3069d61e942545fbb5fbf1efdddd8285
orwoods <vanya@techjedi.co>
2024-11-15 18:48:32 +0100

    test

--- src/grpc/server.ts
+++ src/grpc/server.ts
@@ -11,18 +11,25 @@ export { ServerOptions };
 
 export abstract class GrpcServer <SI extends UntypedServiceImplementation> {
   protected server!: Server;
+  private methods?: SI;
 
-  public async init (service: ServiceDefinition<SI>, options?: ServerOptions, logger: TLogger = console) {
+  protected async init (service: ServiceDefinition<SI>, options?: ServerOptions, logger: TLogger = console) {
     const { host, port, tls } = await this.getProps();
 
     const url = `${host}:${port}`;
 
     logger.info('GrpcServer init', { url, tls, options });
 
+    if (!this.methods) {
+      logger.info('Skip init');
+
+      return;
+    }
+
     const credentials = Number(tls) ? ServerCredentials.createSsl(null, [], false) : ServerCredentials.createInsecure();
 
     this.server = new Server(options);
-    this.server.addService(service, this.getImplementation());
+    this.server.addService(service, this.methods);
     this.server.bindAsync(url, credentials, (error) => {
       if (error) {
         logger.error('GrpcServer error', error);
@@ -34,7 +41,9 @@ export abstract class GrpcServer <SI extends UntypedServiceImplementation> {
     });
   }
 
-  protected abstract getImplementation (): SI;
+  protected registerMethods (methods: SI) {
+    this.methods = methods;
+  }
 
   abstract getProps(): Promise<{ host: string; port: number; tls: boolean }>;
 }
