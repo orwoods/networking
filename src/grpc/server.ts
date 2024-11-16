1fba2245a611bf7acd5fba65e8fdd251537029e1
orwoods <vanya@techjedi.co>
2024-11-16 11:08:02 +0100

    GrpcServer update

--- src/grpc/server.ts
+++ src/grpc/server.ts
@@ -6,43 +6,60 @@ import {
   UntypedServiceImplementation,
 } from '@grpc/grpc-js';
 import { TLogger } from '../types';
+import { wait } from '../utils';
 
 export { ServerOptions };
 
-export abstract class GrpcServer <SI extends UntypedServiceImplementation> {
-  protected server!: Server;
-  private methods?: SI;
+export abstract class GrpcServer <IMethods extends UntypedServiceImplementation, IService extends ServiceDefinition<IMethods>> {
+  protected server: Server;
+  private logger: TLogger;
+  private activePort?: string;
 
-  protected async init (service: ServiceDefinition<SI>, options?: ServerOptions, logger: TLogger = console) {
+  public constructor (service: IService, methods: IMethods, options?: ServerOptions, logger: TLogger = console) {
+    this.server = new Server(options);
+    this.server.addService(service, methods);
+    this.logger = logger;
+  }
+
+  protected async start () {
     const { host, port, tls } = await this.getProps();
 
     const url = `${host}:${port}`;
-
-    logger.info('GrpcServer init', { url, tls, options });
-
-    if (!this.methods) {
-      logger.info('Skip init');
-
-      return;
-    }
-
     const credentials = Number(tls) ? ServerCredentials.createSsl(null, [], false) : ServerCredentials.createInsecure();
 
-    this.server = new Server(options);
-    this.server.addService(service, this.methods);
+    await this.stop();
+
+    this.activePort = String(port);
+
+    let success: (value: void | PromiseLike<void>) => void;
+    let failure: (reason?: any) => void;
+
+    const promise = new Promise<void>((resolve, reject) => {
+      success = resolve;
+      failure = reject;
+    });
+
     this.server.bindAsync(url, credentials, (error) => {
       if (error) {
-        logger.error('GrpcServer error', error);
+        this.logger.error('GrpcServer error', error);
 
-        return;
+        return failure();
       }
 
-      logger.info('GrpcServer server started');
+      this.logger.info('GrpcServer server started');
+
+      success();
     });
+
+    return promise;
   }
 
-  protected registerMethods (methods: SI) {
-    this.methods = methods;
+  protected async stop () {
+    if (this.activePort) {
+      this.server.drain(this.activePort, 1000);
+      await wait(1000);
+      this.server.unbind(this.activePort);
+    }
   }
 
   abstract getProps(): Promise<{ host: string; port: number; tls: boolean }>;
